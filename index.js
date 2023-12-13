67c692f17c7ec5bf1ab0aa7f1d9ce6f11ce56b0c
kinsiabdirahman <kinsiabdirahman@outlook.com>
2023-12-13 22:46:07 +0300

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+
+// Function expression called divide
+const divide = () => {
+  return 2000 / 100;
+};
+
+// Arrow function called square
+const square = (num) => {
+  return num * num;
+};
+
+// Arrow function called add
+const add = (num1, num2) => {
+  return num1 + num2;
+};
